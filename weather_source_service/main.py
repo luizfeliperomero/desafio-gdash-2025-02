b6b269f675c0ad1c8867e419e1e580d4765ab778
luizf <luizfeliperomero10@gmail.com>
2025-11-22 00:00:26 -0300

    Add heartbeat update

--- weather_source_service/main.py
+++ weather_source_service/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from datetime import datetime, timedelta
 from dataclasses import dataclass
 from threading import Thread
+from pydantic import BaseModel
 import requests
 import schedule
 import time
@@ -17,20 +18,36 @@ Location = tuple[float, float]
 
 ## TODO: Make it atomic (luizf)
 locations: dict[Location, Timestamps] = {}
+
+def LocationBody(BaseModel):
+    lat: float
+    lon: float
+
+@app.post("/api/v1/heartbeat")
+def update_heartbeat(body: LocationBody):
+    lat = body.lat
+    lon = body.lon
+    loc = (lat, lon)
+
+
+    if loc in locations:
+        locations[loc].last_heartbeat = now
+    else:
+        create_location(loc)
+    
     
 @app.get("/api/v1/weather")
 def add_location(lat, lon):
-    now = datetime.now();
+    create_location((lat, lon));
+    return get_weather(lat, lon)
 
-    locations[(lat, lon)] = Timestamps(
+def create_location(loc: [float, float]):
+    now = datetime.now()
+
+    locations[loc] = Timestamps(
         last_fetched=now,
         last_heartbeat=now
     )
-
-
-    return get_weather(lat, lon)
-
-    schedule.every().hour.do(get_weather, lat, lon).tag(f"{lat},{lon}")
 
 def scheduler_loop():
     while True:
@@ -61,6 +78,8 @@ def manage_locations():
         if now - job.last_fetched >= one_hour:
             job.last_fetched = now
             get_weather(lat, lon)
+
+    print(locations.keys())
 
 def get_weather(lat, lon):
     api_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
