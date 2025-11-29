692bbf3416b1c089d3888c74003e678d0f2b1395
luizf <luizfeliperomero10@gmail.com>
2025-11-28 23:57:10 -0300

    Add weather ingestion worker

---
+++ weather_ingestion_worker/go.mod
@@ -0,0 +1,10 @@
+module weather-ingestion-worker
+
+go 1.23
+
+require (
+	github.com/klauspost/compress v1.18.1 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/twmb/franz-go v1.20.5 // indirect
+	github.com/twmb/franz-go/pkg/kmsg v1.12.0 // indirect
+)
---
+++ weather_ingestion_worker/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+    "context"
+    "fmt"
+    "log"
+	"net/http"
+    "github.com/twmb/franz-go/pkg/kgo"
+	"bytes"
+)
+
+type WeatherMessage struct {
+    Latitude  float64 `json:"latitude"`
+    Longitude float64 `json:"longitude"`
+    Current   struct {
+        Time              string  `json:"time"`
+        Temperature2m     float64 `json:"temperature_2m"`
+        RelativeHumidity2m int     `json:"relative_humidity_2m"`
+        Precipitation     float64 `json:"precipitation"`
+        WeatherCode       int     `json:"weather_code"`
+        WindSpeed10m      float64 `json:"wind_speed_10m"`
+        IsDay             int     `json:"is_day"`
+    } `json:"current"`
+}
+
+func main() {
+    brokers := []string{"kafka:9092"}
+    topic := "weather"
+
+    client, err := kgo.NewClient(
+        kgo.SeedBrokers(brokers...),
+		kgo.ConsumerGroup("weather-worker-group"),
+        kgo.ConsumeTopics(topic),
+		kgo.ConsumeResetOffset(kgo.NewOffset().AtEnd()),
+    )
+    if err != nil {
+        log.Fatalf("unable to create kafka client: %v", err)
+    }
+    defer client.Close()
+
+    fmt.Println("Kafka consumer started...")
+
+    for {
+        fetches := client.PollFetches(context.Background())
+
+        if errs := fetches.Errors(); len(errs) > 0 {
+            for _, e := range errs {
+                log.Printf("error consuming topic %s: %v", e.Topic, e.Err)
+            }
+            continue
+        }
+
+        fetches.EachRecord(func(record *kgo.Record) {
+			var msg WeatherMessage
+			if err := json.Unmarshal(record.Value, &msg); err != nil {
+				log.Println("JSON decode error:", err)
+				return
+			}
+			jsonBytes, err := json.Marshal(msg)
+			if err != nil {
+				log.Println("JSON encode error:", err)
+				return
+			}
+
+			resp, err := http.Post(
+				"http://weather:3000/weather",
+				"application/json",
+				bytes.NewBuffer(jsonBytes),
+			)
+			if err != nil {
+				log.Println("POST request failed:", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			log.Println("POST OK:", resp.Status)
+        })
+    }
+}
+
